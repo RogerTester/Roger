1a206f91316aa4d5761407f8c3fb94bf6d0fb06b
Chain <qianqian.du@ele.me>
2018-05-17 21:11:30 +0800

    dqq

--- Roger.py
+++ Roger.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
-def hello_world():
-	return render_template('index.html')
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('index.html', name=name)
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
